17e1da365a39b0b89cc02a951d2cd5f3746cfc0b
Hi <huydao1008@gmail.com>
2024-03-14 19:29:21 +0700

    Add Google Fonts package for text styling

--- lib/config/themes/theme.dart
+++ lib/config/themes/theme.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:furnika/config/themes/app_palette.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
   static _border([Color color = Colors.transparent]) => OutlineInputBorder(
@@ -23,5 +24,6 @@ class AppTheme {
       focusedBorder: _border(AppPalette.textFieldStroke),
       errorBorder: _border(AppPalette.error),
     ),
+    textTheme: GoogleFonts.lexendTextTheme(),
   );
 }
